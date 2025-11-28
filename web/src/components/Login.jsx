67a83c853b0cba8b435b09747d40b872fdc4701c
ananya <ananya884.sharma@gmail.com>
2025-11-28 19:50:42 +0530

    login page

--- web/src/components/Login.jsx
+++ web/src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FiShield, FiLock, FiServer } from 'react-icons/fi';
 import { login } from '../services/api';
 
 const Login = ({ onLogin }) => {
